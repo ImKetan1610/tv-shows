a852939a7e51f301b6bf83e49f84614c69215f01
ImKetan1610 <ketan.makode@gmail.com>
2023-03-30 11:18:12 +0530

    updated: css file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import s from "./style.module.css";
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div className={s.main_container}>
+      <div className={s.header}>
+        <div className="row">
+          <div className="col-4">
+            <div>LOGO</div>
+            <div>Subtitle</div>
+          </div>
+          <div className="col-md-12 col-lg-4">
+            <input style={{width:'100%'}} type="text" />
+          </div>
+        </div>
+      </div>
+      <div className={s.tv_show_detail}>TV Show Detail</div>
+      <div className={s.recommended_tv_shows}>Recommended TV Shows</div>
     </div>
   );
 }
