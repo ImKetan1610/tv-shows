3af294753d6f6572866a4027c66b9dc8cca0c82a
ImKetan1610 <ketan.makode@gmail.com>
2023-05-11 12:17:21 +0530

    cleaning the search bar after searching for the show

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./style.module.css";
 import { Search as SearchIcon } from "react-bootstrap-icons";
 
 export const SearchBar = ({ onSubmit }) => {
+  const [value, setValue] = useState("");
+
   function submit(e) {
     if (e.key === "Enter" && e.target.value.trim() !== "") {
       onSubmit(e.target.value);
+      setValue("");
     }
   }
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
 
   return (
     <>
       <SearchIcon size={27} className={s.icon} />
       <input
         onKeyUp={submit}
+        onChange={handleChange}
         className={s.input}
         type="text"
+        value={value}
         placeholder={"Search a tv show you may like"}
       />
     </>
